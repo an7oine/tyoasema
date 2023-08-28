e99d02472d436a81b914d35d50d6cb074cc1592f
Antti Hautaniemi <antti.hautaniemi@pispalanit.fi>
2023-08-28 12:39:03 +0300

    Zsh: korjattu `koodi`- ja `etsi`-liitännäiset (PEP 660)

--- .oh-my-zsh/custom/plugins/etsi/etsi.plugin.zsh
+++ .oh-my-zsh/custom/plugins/etsi/etsi.plugin.zsh
@@ -1,9 +1,15 @@
 function etsi () {
   vi "+/\v${*}" $(
-    (
+    {
+      # Vanhat .egg-link -tyyppiset asennukset.
       find "$( python -c 'print(__import__("sysconfig").get_paths()["purelib"])' )" \
-        -name \*.egg-link -exec head -n1 "{}" \;
-    ) | while read paketti; do
+        -name \*.egg-link -exec head -n1 '{}' \;
+
+      # PEP 660 -yhteensopivat asennukset.
+      find "$( python -c 'print(__import__("sysconfig").get_paths()["purelib"])' )" \
+        -name direct_url.json -exec jq -r .url '{}' \; | sed 's_^file://__'
+
+    } | while read paketti; do
       git -C "$paketti" grep -E --untracked -l -- "${*}" | xargs -I % echo "${paketti}/%"
     done #| paste -s -
   )
--- .oh-my-zsh/custom/plugins/koodi/koodi.plugin.zsh
+++ .oh-my-zsh/custom/plugins/koodi/koodi.plugin.zsh
@@ -1,8 +1,14 @@
 function koodi() {
-  (
+  {
+    # Vanhat .egg-link -tyyppiset asennukset.
     find "$( python -c 'print(__import__("sysconfig").get_paths()["purelib"])' )" \
-      -name \*.egg-link -exec head -n1 "{}" \;
-  ) | while read paketti; do
+      -name \*.egg-link -exec head -n1 '{}' \;
+
+    # PEP 660 -yhteensopivat asennukset.
+    find "$( python -c 'print(__import__("sysconfig").get_paths()["purelib"])' )" \
+      -name direct_url.json -exec jq -r .url '{}' \; | sed 's_^file://__'
+
+  } | while read paketti; do
     find "${paketti}" \
     -not -path \*.eggs/\* \
     -not -name \*neomake\* \
